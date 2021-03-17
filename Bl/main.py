143318debd6d9d4ffd1993eabf9617894ea52628
Shaidullin Ilgam <ilgam.shaidullin@gmail.com>
2021-03-17 23:56:58 +0300

    RV
    Signed-off-by: Shaidullin Ilgam <ilgam.shaidullin@gmail.com>

--- Bl/main.py
+++ Bl/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Ильгама лисичке"
+    return "Привет от приложения Ильгама РВ"
 
 
 if __name__ == '__main__':
